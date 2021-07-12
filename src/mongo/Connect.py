1d599327d628c169ebd2da8f7b56817c37fb8dbe
Shantanu Sood <shantanusood.1989@gmail.com>
2021-07-12 19:41:58 -0400

    Trying to pass heroku build 3

--- src/mongo/Connect.py
+++ src/mongo/Connect.py
@@ -1,9 +1,10 @@
 import configparser
 import pymongo
+import os
 
 def getCollection(collection):
     config = configparser.RawConfigParser()
     config.read('./data/mongo.properties')
-    config.get('Auth_Info', 'user')
-    client = pymongo.MongoClient("mongodb+srv://"+config.get('Auth_Info', 'user')+":"+config.get('Auth_Info', 'pass')+"@cluster0.csbwv.mongodb.net/Cluster0?retryWrites=true&w=majority")
-    return client[config.get('Auth_Info', 'database')][str(collection)]
+    #client = pymongo.MongoClient("mongodb+srv://"+config.get('Auth_Info', 'user')+":"+config.get('Auth_Info', 'pass')+"@cluster0.csbwv.mongodb.net/Cluster0?retryWrites=true&w=majority")
+    client = pymongo.MongoClient(os.environ.get("MONGODB_URI"))
+    return client[config.get('Auth_Info', 'auth_database')][str(collection)]
