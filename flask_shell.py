92ae663acaa9a3b52bed5b2342ab5f9fe40e5106
Shantanu Sood <shantanusood.1989@gmail.com>
2021-08-22 20:28:32 -0400

    Fixed issue with account group not loading 3

--- flask_shell.py
+++ flask_shell.py
@@ -217,15 +217,15 @@ def updateAccounts(username, account1, account2, account3):
 def getAccGroups(username):
     data = json.loads(dumps(con.getCollection("AccGroups").find({"_id": str(username)})))
     acc_data = json.loads(dumps(con.getCollection("Accounts").find({"_id": str(username)})))[0]['accounts']
-    toBeRemoved = set()
+    toBeRemoved = []
     if len(data) > 0:
         for x in data[0]['groups']:
             count = 0
             for y in data[0]['groups'][x]:
                 for z in acc_data:
                     if y not in acc_data:
-                        print(x, y)
-                        toBeRemoved.add([x, y])
+                        if [x, y] not in toBeRemoved:
+                            toBeRemoved.append([x, y])
                     if y['id'] == z['id']:
                         data[0]['groups'][x][count]['name'] = z['name']
                         data[0]['groups'][x][count]['color'] = z['color']
